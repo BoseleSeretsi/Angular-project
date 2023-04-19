47758769a6779df0aacd026bc1a8a966e7d441f2
BoseleSeretsi <129956765+BoseleSeretsi@users.noreply.github.com>
2023-04-19 14:38:47 +0200

    angularbosele

---
+++ angularbosele/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = 
+[{ path: '', component: NavbarComponent },
+{ path: 'home', component: HomepageComponent },
+{ path: 'footer', component: FooterComponent },
+{ path: 'product', component: ProductsComponent },
+{ path: 'cart', component: CardComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angularbosele/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
+import { ProductsComponent } from './products/products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    FooterComponent,
+    CardComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
